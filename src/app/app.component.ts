73b90f3c6ecf2ea30c04dc8a2af0135aa322dbc4
click2cloud-anubh <anubh.gupta@click2cloud.net>
2022-05-26 15:36:21 +0530

    Pipes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { isNgTemplate, ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +9,50 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Template Reference Variable & Basic Pipes';
-  today= Date();
+  // today= Date();
 
-  user= {
-    name:'Anubh',
-    age: 21
+  // user= {
+  //   name:'Anubh',
+  //   age: 21
+  // }
+
+  // getVal(item: HTMLInputElement){
+  //   console.warn(item);
+  // }
+
+  // capString(item: string) {
+  //   return item.toUpperCase();
+  // }
+
+  // userLogin(item: any) {
+  //   console.warn(item);
+  // }
+  // -----------------------------------------------------
+  // loginForm= new FormGroup({
+  //   user: new FormControl(''),
+  //   password: new FormControl('')
+  // });
+
+  // loginUser() {
+  //   console.warn(this.loginForm.value);
+  // }
+
+  // ----------------------------------------------------------
+
+  loginForm= new FormGroup({
+    // Validators.email, 
+    user: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+    password: new FormControl('',  [Validators.required, Validators.minLength(5)])
+  })
+
+  loginUser() {
+    console.warn(this.loginForm.value);
   }
 
-  getVal(item: HTMLInputElement){
-    console.warn(item);
+  get user() {
+    return this.loginForm.get('user');
   }
-
-  capString(item: string) {
-    return item.toUpperCase();
+  get password() {
+    return this.loginForm.get('password');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ChildComponent } from './child/child.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { Child2Component } from './child2/child2.component';
 import { UsdInrPipe } from './pipes/usd-inr.pipe';
+import { RedElDirective } from './red-el.directive';
 
 @NgModule({
   declarations: [
@@ -20,10 +21,12 @@ import { UsdInrPipe } from './pipes/usd-inr.pipe';
     UserDetailsComponent,
     Child2Component,
     UsdInrPipe,
+    RedElDirective,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule,
     BrowserAnimationsModule
   ],
